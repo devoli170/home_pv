2afa89e03b792e0dd67b136d878dc288ccdbd12e
Oli <root@DESKTOP-BIQD8GH.localdomain>
2021-05-23 17:32:41 +0200

    moving to OOP apporach, non functional changeset, need some ideas for FSM

--- solar_control/main_module/ladex10.py
+++ solar_control/main_module/ladex10.py
@@ -3,119 +3,67 @@ from datetime import datetime
 from solar_control.io_wrapper import GPIO
 
 
-class OutPin:
-    modus = ""
-    name = ""
-    nummer = None
-    wert = None
-
-    def __init__(self, name, nummer, modus, wert):
+class Pin:
+    def __init__(self, name, nummer, modus):
         self.name = name
         self.nummer = nummer
         self.modus = modus
+        GPIO.setup(self.nummer, self.modus)
+
+
+class OutPin(Pin):
+    def __init__(self, name, nummer, modus, wert):
+        Pin.__init__(name, nummer, modus)
         self.wert = wert
+        self.schreibe_wert_an_pin()
+
+    def schreibe_wert_an_pin(self):
+        GPIO.output(self.nummer, self.wert)
 
 
-HIGH = GPIO.LOW
-LOW = GPIO.HIGH
-solar_l1 = OutPin("solar_l1", 17, "out", LOW)
-solar_n = OutPin("solar_n", 18, "out", LOW)
-haus_l1 = OutPin("haus_l1", 22, "out", LOW)
-haus_n = OutPin("haus_n", 23, "out", LOW)
-out_pins = [solar_l1, solar_n, haus_l1, haus_n]
-optokoppler = 24
-input_pins = [optokoppler]
+class InPin(Pin):
+    def __init__(self, name, nummer, modus):
+        Pin.__init__(name, nummer, modus)
+
+    def lese_pin_wert(self):
+        return GPIO.input(self.nummer)
 
 
-def init():
-    print("starte init")
-    for pin in out_pins:
-        GPIO.setup(pin.nummer, GPIO.OUT)
-    GPIO.setup(input_pins, GPIO.IN)
-    schreibe_wert_auf_pin()
-    sleep(2)
-    solarstrom = GPIO.input(optokoppler)
-    if solarstrom == GPIO.HIGH:
-        schnell_start()
-    print("init fertig")
+class StromKontrolle:
+    def __init__(self, solar_pins, hausstrom_pins, in_pin, solar_wechsel_wartezeit ):
+        self.solar_pins = solar_pins
+        self.hausstrom_pins = hausstrom_pins
+        self.in_pin = in_pin
+        self.solar_wechsel_wartezeit = solar_wechsel_wartezeit
+        self.jetzt_geschalten = "nichts"
+
+    def lese_jetzige_schaltung(self):
 
 
-def schnell_start():
-    print("schalte hausstrom aus")
-    hausstrom_aus()
-    print("schalte solarstrom an")
-    solar_l1.wert = HIGH
-    solar_n.wert = HIGH
-    schreibe_wert_auf_pin()
 
 
-def hausstrom_aus():
-    print("schalte hausstrom aus")
-    haus_l1.wert = LOW
-    haus_n.wert = LOW
-    schreibe_wert_auf_pin()
-    sleep(2)
+    def evaluiereZustand(self):
+        self.in_pin.lese_pin_wert()
 
-
-def solarstrom_aus():
-    print("solarstrom aus")
-    solar_l1.wert = LOW
-    solar_n.wert = LOW
-    schreibe_wert_auf_pin()
-    sleep(2)
-
-
-def hausstrom_an():
-    if haus_l1.wert != HIGH and haus_n.wert != HIGH:
-        solarstrom_aus()
-        print("schalte hausstrom an")
-        haus_l1.wert = HIGH
-        haus_n.wert = HIGH
-        schreibe_wert_auf_pin()
-    else:
-        print("hausstrom ist an")
-
-
-#def solarstrom_an():
-#    if solar_l1.wert != HIGH and solar_n.wert != HIGH:
-#        print(datetime.now())
-#        print("warte 10 minuten bis solarstrom angeschaltet wird")
-#        sleep(600)
-#        print(datetime.now())
-#        hausstrom_aus()
-#        print("schalte solarstrom an")
-#        solar_l1 = HIGH
-#        solar_n = HIGH
-#        schreibe_wert_auf_pin()
-#    else:
-#        print("solarstrom ist an")
-
-
-def schreibe_wert_auf_pin():
-    for pin in out_pins:
-        GPIO.output(pin.nummer, pin.wert)
-
+    def start(self):
+        while(True):
+            evaluiereZustand():
 
 def beep():
     return "beep"
 
 
 def main():
-    init()
-    '''
-    while true;
-do
-  solarstrom=$(gpio -g read 24)
-  if (( $solarstrom == 1 ))
-  then
-    solarstrom_an
-    sleep 1
-  else
-    hausstrom_an
-    sleep 1
-  fi;
-done
-'''
+    solar_l1 = OutPin("solar_l1", 17, "out", GPIO.LOW)
+    solar_n = OutPin("solar_n", 18, "out", GPIO.LOW)
+    haus_l1 = OutPin("haus_l1", 22, "out", GPIO.LOW)
+    haus_n = OutPin("haus_n", 23, "out", GPIO.LOW)
+    solar_pins = [solar_n, solar_l1]
+    hausstrom_pins = [haus_n, haus_l1]
+    optokoppler = InPin("optokoppler", 24, "in")
+    sleep(2)
+    strom_kontrolle = StromKontrolle(solar_pins, hausstrom_pins, optokoppler, 600)
+    strom_kontrolle.start()
 
 
 if __name__ == "__main__":
